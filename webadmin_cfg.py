6314844c81dfaaedef3a36057766404e37043a01
Tomáš Diviš <tomas.divis@nic.cz>
2008-03-28 15:52:19 +0000

    Upraveny cesty v konfiguraku (diky tomu funguje curdir)
    git-svn-id: file:///data/svn/enum/webadmin/trunk@5123 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- webadmin_cfg.py
+++ webadmin_cfg.py
@@ -1,15 +1,15 @@
 debug = True
-caching_filter_form_javascript = True # if this settings is on, then when doing changes to filter form, you should erase all sessions, because old filter forms are still in session
+caching_filter_form_javascript = False # if this settings is on, then when doing changes to filter form, you should erase all sessions, because old filter forms are still in session
 
 
 def curdir(f = ''):
     import os.path
     return os.path.join(os.path.dirname(__file__), f) # in globlal module builtin variable __file__ is path, from which was program executed
-#base_dir = curdir()
-base_dir = '/home/glin/programming/workspace/webadmin'
-www_dir = base_dir + "/www"
-sessions_dir = base_dir + '/sessions'
-locale_dir = base_dir + '/locale'
+base_dir = curdir()
+#base_dir = '/home/glin/programming/workspace/webadmin/'
+www_dir = base_dir + "www/"
+sessions_dir = base_dir + 'sessions/'
+locale_dir = base_dir + 'locale/'
 log_dir = base_dir
 
 #idl = '/home/glin/programming/svn_enum_checkout/enum/idl/trunk/ccReg.idl'
@@ -45,7 +45,7 @@ cherrycfg = {
         'tools.sessions.storage_path': sessions_dir,
         'tools.sessions.timeout': 60, # in minutes
         'server.log_to_screen': False,
-        'server.log_file': log_dir + '/fred_webadmin.log',
+        'server.log_file': log_dir + 'fred_webadmin.log',
     },
     '/': {'tools.staticdir.root':  www_dir},
     '/css': {'tools.staticdir.on': True,
@@ -55,7 +55,7 @@ cherrycfg = {
     '/img': {'tools.staticdir.on': True,
              'tools.staticdir.dir': 'img'},
     '/favicon.ico': {'tools.staticfile.on': True,
-                     'tools.staticfile.filename': www_dir + '/img/favicon.png'}
+                     'tools.staticfile.filename': www_dir + 'img/favicon.png'}
 }
 
 
