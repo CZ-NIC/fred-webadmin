e15f2cb702d7a7d7da9e4691cfb648b556f05219
Aleš Doležal <ales.dolezal@nic.cz>
2008-06-05 08:38:27 +0000

    `fred-webadmin-server` in `init.d` directory is now executable after install.
    git-svn-id: file:///data/svn/enum/webadmin/trunk@5568 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- freddist/command/install_data.py
+++ freddist/command/install_data.py
@@ -204,7 +204,7 @@ class install_data(_install_data, install_parent):
                                     os.path.join(self.srcdir, data))
                         (out, _) = self.copy_file(data, dir)
                         self.outfiles.append(out)
-                        if 'bin' in out.split(os.path.sep) or 'sbin' in out.split(os.path.sep):
+                        if 'bin' in out.split(os.path.sep) or 'sbin' in out.split(os.path.sep) or 'init.d' in out.split(os.path.sep):
                             os.chmod(out, 0755)
 
                         if out.endswith('.py') and not self.dont_create_pycpyo:
