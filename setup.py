0efdb5a2c9a90bcc46ec473f899f094b0c70343a
Jaromír Talíř <jaromir.talir@nic.cz>
2008-09-26 14:59:49 +0000

    relase 3.0.1 preparation
    git-svn-id: file:///data/svn/enum/webadmin/trunk@6469 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def njoin(*args):
 
 PROJECT_NAME = 'fred-webadmin'
 PACKAGE_NAME = 'fred_webadmin'
-PACKAGE_VERSION = '2.1.0'
+PACKAGE_VERSION = '3.0.1'
 
 SHARE_DOC = njoin('share', 'doc', PROJECT_NAME)
 SHARE_PACKAGE = njoin('share', PROJECT_NAME)
