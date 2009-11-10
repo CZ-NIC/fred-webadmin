376ae9ed84d63785b4ad395df7cf38c722367109
Tomáš Brambora <tomas.brambora@nic.cz>
2009-11-10 14:40:02 +0000

    Refactored mapping.
    git-svn-id: file:///data/svn/enum/webadmin/trunk@9415 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- fred_webadmin/mappings.py
+++ fred_webadmin/mappings.py
@@ -6,51 +6,79 @@ from omniORB import CORBA, importIDL
 importIDL(config.idl)
 ccReg = sys.modules['ccReg']
 
-filter_type_items = ccReg.FilterType._items
+def corbaname_to_classname(item):
+    """ Return classname (class name in lowercase) for a given ccReg.FT_FILTER*
+        type. 
+    """
+    rules = {
+        ccReg.FT_STATEMENT_ITEM._v : 'bankstatement'
+    }
+    return rules.get(item._v, item._n[3:].lower())
+    
 
-#dict {classname: enum_item) where enum_item is item in FilterType (from corba) and url is base url of that object  
-f_name_enum = dict([(item._n[3:].lower(), item) for item in filter_type_items])
+filter_type_items = [dict(
+    [
+        ("classname", corbaname_to_classname(item)), 
+        ("item", item)
+    ]) for item in ccReg.FilterType._items]
+
+#dict {classname: enum_item) where enum_item is item in FilterType (from corba)
+# and url is base url of that object  
+f_name_enum = dict([(item['classname'], item['item']) for 
+    item in filter_type_items])
 
 # dict {enum_item: classname}
-f_enum_name = dict([(item, item._n[3:].lower()) for item in filter_type_items])
+f_enum_name = dict([(value, key) for key, value in f_name_enum.items()])
 
-#dict {classname: id_item) where id_item is item in FilterType (from corba) and url is base url of that object  
-f_name_id = dict([(item._n[3:].lower(), item._v) for item in filter_type_items])
+#dict {classname: id_item) where id_item is item in FilterType (from corba) and 
+# url is base url of that object  
+f_name_id = dict([(item['classname'], item['item']._v) for 
+    item in filter_type_items])
 
 # dict {id_item: classname}
-f_id_name = dict([(item._v, item._n[3:].lower()) for item in filter_type_items])
+f_id_name = dict([(value, key) for key, value in f_name_id.items()])
 
 # dict {enum_item: url} 
 f_urls = dict([(name, '/%s/' % (name)) for name in f_name_enum.keys()])
 
-# dict {classname: CT_*_ID}, where * is uppercase classname (usid in itertable headers for 'Id' column)
-f_header_ids = dict([(name, 'CT_%s_ID' % (name.upper())) for name in f_name_enum.keys()])
+# dict {classname: CT_*_ID}, where * is uppercase classname (used in itertable
+# headers for 'Id' column)
+f_header_ids = dict([(name, 'CT_%s_ID' % (name.upper())) for 
+    name in f_name_enum.keys()])
 
 # dict {OT_*, classname}, where OT_* is from _Admin.idl ObjectType 
-f_objectType_name = dict([(item, item._n[3:].lower()) for item in ccReg.PublicRequest.ObjectType._items])
+f_objectType_name = dict([(item, item._n[3:].lower()) for 
+    item in ccReg.PublicRequest.ObjectType._items])
 
-f_name_filterformname = dict([(item._n[3:].lower(), item._n[3:].capitalize() + 'FilterForm') for item in filter_type_items])
-f_name_filterformname['nsset'] = 'NSSetFilterForm' 
-f_name_filterformname['keyset'] = 'KeySetFilterForm' 
-f_name_filterformname['publicrequest'] = 'PublicRequestFilterForm' 
+def generate_dict(suffix):
+    rules = {
+        'nsset' : 'NSSet',
+        'keyset' : 'KeySet',
+        'publicrequest' : 'PublicRequest',
+        'bankstatement' : 'Bankstatement'
+    }
+    result = dict(
+        [
+            (
+                item['classname'], 
+                rules.get(
+                    item['classname'], item['classname'].capitalize()) + suffix)
+            for item in filter_type_items
+        ])
+    return result
 
-f_name_editformname = dict([(item._n[3:].lower(), item._n[3:].capitalize() + 'EditForm') for item in filter_type_items])
-f_name_editformname['nsset'] = 'NSSetEditForm' 
-f_name_editformname['keyset'] = 'KeySetEditForm' 
-f_name_editformname['publicrequest'] = 'PublicRequestEditForm'
+f_name_filterformname = generate_dict('FilterForm')
+f_name_editformname = generate_dict('EditForm')
+f_name_detailname = generate_dict('Detail')
 
-f_name_detailname = dict([(item._n[3:].lower(), item._n[3:].capitalize() + 'Detail') for item in filter_type_items])
-f_name_detailname['nsset'] = 'NSSetDetail' 
-f_name_detailname['keyset'] = 'KeySetDetail' 
-f_name_detailname['publicrequest'] = 'PublicRequestDetail'
-
-f_name_actionname =  dict([(item._n[3:].lower(), item._n[3:].capitalize()) for 
-                           item in filter_type_items])
-f_name_actionname['nsset'] = 'NSSet'
+f_name_actionname = generate_dict('')
+# Overwrite some remaining non-matching (class -> action) mappings.
+# This is necessary because of tight coupling between class names and action
+# types. And names used in Corba would be weird as Python class names here in
+# webadmin.
+# Possible TODO(tomas): refactor to loosen the coupling.
 f_name_actionname['mail'] = 'Emails'
-f_name_actionname['keyset'] = 'KeySet' 
 f_name_actionname['action'] = 'Actions'
-f_name_actionname['publicrequest'] = 'PublicRequest'
 f_name_actionname['logger'] = 'Request'
 
 f_name_actionfiltername = dict([(key, value + 'Filter') for key, value in
@@ -74,9 +102,6 @@ f_name_default_sort = {
     'contact': [['Handle', 'ASC'], ['Create date', 'DESC']],
     'nsset': [['Handle', 'ASC'], ['Create date', 'DESC']],
     'keyset': [['Handle', 'ASC'], ['Create date', 'DESC']],
-    #'domain': [['Create date', 'ASC']],
-    #'domain': [['Create date', 'DESC']],
-    #'domain': [['FQDN', 'ASC'], ['Create date', 'ASC']],
     'domain': [['FQDN', 'ASC'], ['Create date', 'DESC']],
     'action': [['Time', 'DESC']],
     'invoice': [['Create Date', 'DESC']],
