e52cba3ed4da51a4467b2af034f51014c665b8a1
Tomáš Brambora <tomas.brambora@nic.cz>
2010-06-08 14:10:48 +0000

    Added code doc.
    git-svn-id: file:///data/svn/enum/webadmin/trunk@12113 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- www/js/check_filter_forms_javascript.js
+++ www/js/check_filter_forms_javascript.js
@@ -12,28 +12,34 @@ function checkFilterFormsJavascriptLoaded() {
         }
 }
 
+/* Generate choices for ActionType filter field in log request filter form. */
 function filter_action_types() {
     var action_select = document.getElementById("logger_action_type_id");
     if (action_select == null) {
+        /* ActionType field is not active => do nothing. */
         return;
     }
     var service_select = document.getElementById("logger_service_type_id");
     var actions = []
     var show_all = service_select == null || service_select.selectedIndex == 0
     if (show_all) {
+        /* Glue together all the choices for every service type and display
+         * them all. */
         var actions_by_types = get_actions();
         for (a in actions_by_types) {
             actions = actions.concat(actions_by_types[a])
         }
     } else {
-        var index = service_select.selectedIndex - 1; // Minus one for the empty type.
+        /* Only display the actions for the given service type. */
+        // Minus one for the empty type.
+        var index = service_select.selectedIndex - 1; 
         var actions_by_types = get_actions();
         actions = actions_by_types[index]
     }
+    /* Delete all the choices except the empty value. */
     action_select.length = 1;
-    if (typeof(actions) == "undefined") {
-        return;
-    }
+
+    /* Display the action choices. */
     for (var i=0; i<actions.length; ++i) {
         var newOption = document.createElement('option');
         action_select.add(newOption, null);
