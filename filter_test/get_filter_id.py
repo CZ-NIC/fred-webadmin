9c4ab700594ba6f20119401b31c17e6ad435fb41
Juraj Viceník <juraj.vicenik@nic.cz>
2011-04-19 10:08:04 +0000

    Ticket #5358 - test Id filter on requests
    git-svn-id: file:///data/svn/enum/webadmin/trunk@16737 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ filter_test/get_filter_id.py
@@ -0,0 +1,47 @@
+from omniORB.any import from_any
+
+from fred_webadmin.corba import Corba, ccReg, Registry 
+from fred_webadmin.corbarecoder import CorbaRecode
+
+import pdb;
+
+recoder = CorbaRecode('utf-8')
+c2u = recoder.decode # recode from corba string to unicode
+u2c = recoder.encode # recode from unicode to strings 
+
+corba = Corba()
+# corba.connect('pokuston', 'fred')
+corba.connect('localhost:24846', 'fred')
+
+
+a = corba.getObject('Admin', 'ccReg.Admin')
+s=a.getSession(a.createSession('helpdesk'))
+
+loggerf = s.getPageTable(ccReg.FT_LOGGER)
+lfilter = loggerf.add()
+
+loggerf.setTimeout(0)
+
+
+print 'nastavuji filtery'
+
+lfilter.addId()._set_value(50003633083);
+
+#     pdb_trace()
+print 'pred reloadF()'
+loggerf.reload()
+print 'po reloadF()'
+#     pdb_trace()
+
+print 'HEADERS:'
+print loggerf.getColumnHeaders();
+#
+#print '---RADKY(celkem:%s):' % loggerf._get_numRows()
+#
+for i in range(loggerf._get_numRows()):
+    print loggerf.getRow(i);
+
+print '---KONEC'
+
+
+
