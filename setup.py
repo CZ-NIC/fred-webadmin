02a414496ce0b74f03f9fceefd614483c386ee2a
jsadek <jsadek@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-10-18 09:53:05 +0000

    version bump
    git-svn-id: file:///data/svn/enum/webadmin/trunk@19027 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def njoin(*args):
 
 PROJECT_NAME = 'fred-webadmin'
 PACKAGE_NAME = 'fred_webadmin'
-PACKAGE_VERSION = '3.1'
+PACKAGE_VERSION = '3.7.0'
 
 SHARE_DOC = njoin('share', 'doc', PROJECT_NAME)
 SHARE_PACKAGE = njoin('share', PROJECT_NAME)
